523bc607743b79b4f2cc2d08ea6a3d7c9c4a5195
aettic <aettic@gmail.com>
2022-03-08 12:16:49 -0600

    brainstorming

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python
+
 # Slither v0.1
 
-print("Hello World");
+# Brainstorming goals for this project:
+    # Slither can be just about anything, and in fact it can be multiple things.
+
+
+import sys
+
+def printArgs():
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    print ('Argument List:', str(sys.argv))
+
+
+def doSomething():
+    print("I did something!");
+
+if __name__ == "__main__":
+    print("Slither!");
+
+    # printArgs()
+    doSomething()
