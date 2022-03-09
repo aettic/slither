0b83e37df72953b2cd0924cc4e44fb94a3f79498
aettic <aettic@gmail.com>
2022-03-09 12:58:44 -0600

    Added descriptions for all zone functions.

--- main.py
+++ main.py
@@ -63,26 +63,49 @@ def doSomething(pc):
 
 def gameloop(pc):
     running = pc.isAlive
+
+    # GAME START - should only play when game is launched
+    gameStart(pc)
+    print("The carriage drops you off, and the dust drifts thoughtlessly into the air leaving a stream of beige behind the team of horses and their dark wagon as the driver leaves you here alone. It is quiet.")
+    dirtRoad(pc)
+
     while(running):
 
         if(pc.zone.zoneID == 0):
-            print("The carriage drops you off, and the dust drifts thoughtlessly into the air leaving a stream of beige behind the team of horses and their dark wagon as the driver leaves you here alone. It is quiet.")
-            doSomething(pc)
+            print("")
+            dirtRoad(pc)
 
         elif(pc.zone.zoneID == 1):
             print("Before you stands a looming farmhouse, drab in its aged appearance. The darkened logs and fogged windows belie the tales of magic and adventure you have heard from your friend who is supposed to live here. You visited once, long ago, but that is now a mere memory, and what lies ahead are the echoes of a recent past, one which you do not remember. Something happened here. Where did she go? You recall the note from your friend which you tucked away in your backpack.")
-            doSomething(pc)
+            farmhouseFront(pc)
 
-        else:
+        elif(pc.zone.zoneID == 2):
+            print("")
+            farmhouseKitchen(pc)
+
+        elif(pc.zone.zoneID == 3):
+            print("")
+            farmhouseCloset1(pc)
+
+        elif(pc.zone.zoneID == 4):
+            print("")
+            farmhouseSittingRoom(pc)
+
+        elif(pc.zone.zoneID == 5):
+            print("")
+            farmhouseStairsInside(pc)
+
+        else:  # kill game
             running = False
 
 
 
 
 
-### GAME START
+### DIRT ROAD
 
 def dirtRoadStart(pc):
+    doSomething(pc)
     continue
     # Introduction, carriage leaves
     # Cycle: Walk toward house, walk north or south (null), pick up hat
@@ -117,7 +140,8 @@ def farmhouseSittingRoom(pc):
     continue
     # Smoldering embers of a fire. A couple books. More signs of struggle. Weapon missing from wall.
     # Cycle: Walk to kitchen, upstairs
-    # Item: Books
+    # Item: Books (non removable)
+    # GS: Books read
 
 def farmhouseCloset1(pc):
     continue
@@ -192,6 +216,7 @@ def prairieBackyard(pc):
     continue
     # Open, view of scarecrow, leads to all other places (HUB)
     # Cycle: Well, Shed, Outhouse, barnFront, House Front, Cellar stairs, Cornfield
+    # GS: Bloodtrail examined (look)
 
 def prairieWell(pc):
     continue
@@ -210,6 +235,7 @@ def prairieShedInterior(pc):
     # Filled with tools
     # Cycle: Shed exterior
     # Item: Tool of some kind?
+    # GS: Tool taken
 
 def prairieOuthouse(pc):
     continue
@@ -229,15 +255,28 @@ def barnFront(pc):
 
 def barnInterior(pc):
     continue
+    # Floor made of hard packed dirt with scattered hay. A trail of blood leads out of one of the stables. Pitchfork on a support beam. No animals.
+    # Cycle: barnFront, barnBack, barnStable, barnLoft
+    # Item: Pitchfork (weapon)
+    # GS: Pitchfork taken
 
 def barnLoft(pc):
     continue
+    # Upper area, accessible by ladder from barnInterior. Stray cat startled and reflexes
+    # Cycle: barnInterior
+    # GS: Cat fled
 
 def barnBack(pc):
     continue
+    # Outside the barn's back door, blood trail continues around the edges of the fields all the way to the cornfieldEdge
+    # Cycle: barnInterior, barnStable, prairieBackyard
+    # GS: Bloodtrail examined (automatically True)
 
 def barnStable(pc):
     continue
+    # The rear of the interior, no animals present, but one stable shows clear signs of a struggle. Bloodtrail leads from the stable out into barnBack, and then prairieBackyard, as well as to cornfieldEdge
+    # Cycle: barnBack, barnInterior
+    # GS: Bloodtrail examined (automatically True)
 
 
 
@@ -247,45 +286,78 @@ def barnStable(pc):
 
 def cornfieldEdge(pc):
     continue
+    # The south end of the Cornfield, a wall of tall corn. On the far West side, a trail of blood can be seen once Bloodtrail examined is True.
+    # Cycle: back to prairieBackyard, or prairieOuthouse, forward to cornfieldThick
+    # GS: Bloodtrail examined (look)
 
 def cornfieldThick(pc):
     continue
+    # First step into the corn. The corn is thicker and harder to move through
+    # Cycle: Back to cornfieldEdge, forward to cornfieldTangle
+
 
 def cornfieldTangle(pc):
     continue
+    # The corn is dense, and tangled, but there is a flash of movement
+    # Cycle: Back to cornfieldThick. following the movement leads to cornfieldMazeStart.
+    # GS: Flash of movement
+
 
 def cornfieldMazeStart(pc):
     continue
+    # The corn thins out and opens into a clear path, two and a half feet wide. Plenty of space to walk comfortably.
+    # Cycle: back to cornfieldTangle, forward to cornfieldMaze1
 
 def cornfieldMaze1(pc):
     continue
+    # It becomes apparent that this is a corn maze, because the path branches into a T. Each maze leads to the next, and it's random which one it spits out to
+    # Cycle: Right, Forward, Back
 
 def cornfieldMaze2(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Forward, Left, Back
 
 def cornfieldMaze3(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Right, Left, Back
 
 def cornfieldMaze4(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Left 1, Left 2, Back
 
 def cornfieldMaze5(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Right 1, Right 2, Back
 
 def cornfieldMaze6(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Right, Forward, Left, Back
 
 def cornfieldMaze7(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Right, Left, Back
 
 def cornfieldMaze8(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Right 1, Right 2, Back
 
 def cornfieldMaze9(pc):
     continue
+    # More maze. Each maze leads to the next, and it's random which one it spits out to.
+    # Cycle: Forward, Left, Back
 
 def cornfieldMazeCenter(pc):
     continue
+    # The maze winds up eventually (4 - 15 moves) at the cornfieldMazeCenter, which is an open space. The dirt floor is a perfect circle inside the corn - almost like a crop circle, but in the middle is a spiral staircase leading down. It's important that getting here feels disorienting, and feels like a threshold has been crossed, leading to another world. This change can happen during the above maze instances, gradually. Will need 4 major progression dialogues.
+    # Cycle: Back (immediately leads to cornfieldTangle, but coming back here involves going through the maze again). Down the stairs.
+    # GS: Staircase examined (look)
 
 
 
@@ -315,9 +387,8 @@ if __name__ == "__main__":
     ### Objectives:
     # - Learn to render a window
     # - Learn to output text to that window
-    # - Create a small fantasy world
+    # --- Create a small fantasy world ---
     # - Construct elements and objects for playing the Game
-    # - Develop or steal an input method for text based input
     # - Have fun
 
     ### World / Story creation
